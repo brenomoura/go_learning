b1e033dd013f1f3572f54affb9a8441147b7fb77
Breno Moura <breno.moura@birmind.com.br>
2021-06-07 14:19:10 -0300

    Code refactoring

--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const helloPrefixEnglish = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name + "!"
+	if name == "" {
+		name = "world"
+	}
+	return helloPrefixEnglish + name + "!"
 }
 
 func main() {
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Chris")
-	expected := "Hello, Chris!"
-
-	if result != expected {
-		t.Errorf("Result '%s', expected '%s'", result, expected)
+	checkCorrectMessage := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("Result '%s', expected '%s'", result, expected)
+		}
 	}
+
+	t.Run("Say hello to the user", func(t *testing.T) {
+		result := Hello("Chris")
+		expected := "Hello, Chris!"
+		checkCorrectMessage(t, result, expected)
+	})
+
+	t.Run("Say 'Hello, world!' when an empty string was provided", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, world!"
+		checkCorrectMessage(t, result, expected)
+	})
 }
